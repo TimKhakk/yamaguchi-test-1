0f73f23987be976b4b9d6531b3087f4a964acdfa
Timur Khakkiev <khakkievtimur@yandex.ru>
2021-10-07 15:21:43 +0300

    Добавил шрифт

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 const topPart = document.querySelector(".img-block__top-part");
 const buttonUp = document.querySelector(".img-block__button-up");
 const buttonDown = document.querySelector(".img-block__button-down");
-console.log(buttonDown, buttonUp);
+
 buttonUp.addEventListener('click', () => {
   topPart.className = 'img-block__top-part go-up'
 })
