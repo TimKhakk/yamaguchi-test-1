3971a6bb4131302a06905121b9e6117bae041f27
Timur Khakkiev <khakkievtimur@yandex.ru>
2021-10-07 14:53:55 +0300

    Проект готов

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+const topPart = document.querySelector(".img-block__top-part");
+const buttonUp = document.querySelector(".img-block__button-up");
+const buttonDown = document.querySelector(".img-block__button-down");
+console.log(buttonDown, buttonUp);
+buttonUp.addEventListener('click', () => {
+  topPart.className = 'img-block__top-part go-up'
+})
+
+buttonDown.addEventListener('click', () => {
+  topPart.className = 'img-block__top-part go-down'
+})
